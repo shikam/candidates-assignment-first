61db1274fa83a7c5601c826dff954c9733baf748
shikam <kamenker92@gmail.com>
2019-08-23 18:35:25 +0300

    Added exercise4-6

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+ssh-keygen
+ssh-copy-id vagrant@192.168.100.10
+ssh -o "StrictHostKeyChecking=no" vagrant@server1
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+if [ $# < 2 ]
+then
+    echo "Need insert more 1 parameters"
+    exit 2
+fi
+
+ARGS=("$@")
+counter=0
+destination=${ARGS[-1]}
+for file in "${ARGS[@]}";
+do
+sudo scp $file vagrant@server2:/$destination
+let count = du -sh $file
+let countet = $(($counter + $count))
+#counter = counter + count;
+done
+
+echo $counter
