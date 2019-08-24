118288c428bfc1705377350a4250c345f19f3fca
shikam <kamenker92@gmail.com>
2019-08-24 23:55:06 +0300

    I added a fix to Exercise 6

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #add fix to exercise6-fix here
 
-if [ $# < 2 ]
+if [ $# < 2 ];
 then
     echo "Need insert more 1 parameters"
     exit 2
-fi
 
+else
 ARGS=("$@")
 counter=0
 destination=${ARGS[-1]}
 for file in "${ARGS[@]}";
 do
 sudo scp $file vagrant@server2:/$destination
-let count = du -sh $file
+let count = du $file | cut -c 1-1
+#echo $count
 let countet = $(($counter + $count))
 #counter = counter + count;
 done
 
 echo $counter
+fi
